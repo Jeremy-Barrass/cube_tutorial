c4797df8ace2502551223b9f1d222ab79e949577
jeremy-barrass <jeremy.barrass@gmail.com>
2018-06-06 17:03:27 +0100

    Second commit - basic game works.

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{ 
+    public PlayerMovement movement;
+    void OnCollisionEnter(Collision collisionData)
+    {
+        if (collisionData.collider.tag == "Obstacle")
+        {
+            Debug.Log(collisionData.collider.name);
+            movement.enabled = false;
+        }
+    }
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,20 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
 
     public Rigidbody rb;
+    public float forwardForce = 4500f;
+    public float sideForce = 50f;
 
 	// Use this for initialization
 	void Start ()
 	{
-	    rb.AddForce(0, 200, 500);
+        rb.AddForce(0, 0, forwardForce);
 	}
 
     // Update is called once per frame
-    void Update () {
-        rb.AddForce(0, 0, 200 * Time.deltaTime);
-	}
+    void FixedUpdate () {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(0, (sideForce * Time.deltaTime) / 2, 0, ForceMode.VelocityChange);
+        }
+    }
 }
