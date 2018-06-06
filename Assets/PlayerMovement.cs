02a7c9265a7c847430a34b96cb4f42cee5931e2f
jeremy-barrass <jeremy.barrass@gmail.com>
2018-06-06 11:52:48 +0100

    Initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    rb.AddForce(0, 200, 500);
+	}
+
+    // Update is called once per frame
+    void Update () {
+        rb.AddForce(0, 0, 200 * Time.deltaTime);
+	}
+}
